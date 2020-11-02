1da2d05b7a40066eb317e5bf3a4551e54944874a
b10071007 <b10071007@hotmail.com>
2020-11-02 23:12:36 +0800

    Setup arguments for Test.py by argparse and provide the sample for how to use that

--- Test.py
+++ Test.py
@@ -1,8 +1,9 @@
 import os
 import time
 import numpy as np
+import argparse
+from collections import OrderedDict
 import torch
-# import torch.nn as nn
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
@@ -13,7 +14,7 @@ import utils
 import models
 
 
-def test(net, test_Loader, device, display_interval, save_folder, num_classes, iterations):
+def test(net, test_Loader, device, display_interval, out_dir, num_classes, iterations):
     
     print('{}: Start testing  '.format(utils.GetCurrentTime()))
     confusionMatrix = np.zeros((num_classes,num_classes))
@@ -62,8 +63,8 @@ def test(net, test_Loader, device, display_interval, save_folder, num_classes, i
     output = np.concatenate([proba_all, label_all, predict_class], axis=1)
     output = output.astype('float')
 
-    probaMatrixPath = os.path.join(save_folder, "TestProbaMatrix.txt")
-    confMatPath = os.path.join(save_folder, '{}_Err={}.txt'.format("TestConfMat", error))
+    probaMatrixPath = os.path.join(out_dir, "TestProbaMatrix.txt")
+    confMatPath = os.path.join(out_dir, '{}_Err={}.txt'.format("TestConfMat", error))
     
     np.savetxt(probaMatrixPath, output, fmt='%f')  
     np.savetxt(confMatPath, confusionMatrix, fmt='%d', delimiter='\t') 
@@ -85,35 +86,56 @@ def test(net, test_Loader, device, display_interval, save_folder, num_classes, i
     print('=============================\n')
 
 def main():
-        
-    rootPath = "D:/Dataset/Classification/cifar10/"
-    imgPath = rootPath + "_Images/"
-    test_fListPath = rootPath + "test.txt"
+    
+    parser = argparse.ArgumentParser(description='Classification Testing')
 
-    model_name = "WRN_N4_k4"
-    save_folder = "./weights/allTrain/WRN_N4_k4_drop0.5/bs128_ep200_warm5_lr0.1_gamma0.2_wdecay0.0005_nesterov/"
-    model_path = os.path.join(save_folder, model_name + "_Best.pth")
+    # Dataset setting
+    parser.add_argument('-img_dir', help='the directory for images' )
+    parser.add_argument('-fList_test', help='the mappling list for training set' )
+    parser.add_argument('-num_classes', type=int, default=10, help='the number of classes' )
+    parser.add_argument('-out_dir', default='./weights/', help='the output directory (training logs and trained model)')
+    parser.add_argument('-model_path', default='None', help='the output directory (training logs and trained model)')
+    
+    parser.add_argument('-channel_mean', type=float, nargs='+', default=(0.4914, 0.4822, 0.4465), 
+                        help='the channel mean of images for training set')
+    parser.add_argument('-channel_std', type=float, nargs='+', default=(0.2023, 0.1994, 0.2010), 
+                        help='the channel standard deviation of images for training set')
 
-    num_classes = 10
-    batch_size_test = 100
-    display_interval = 40 
+    # Testing setting
+    parser.add_argument('-gpu_id', default='0', help='setup visible gpus, for example 0,1')
+    parser.add_argument('-model_name', help='the classification model')
+    parser.add_argument('-batch_size_test', type=int, default=64, help='the batch size for testing')
+    parser.add_argument('-display_interval', type=int, default=100,  help='the total training epochs')
+
+    args = parser.parse_args()
+    
+    # rootPath = "D:/Dataset/Classification/cifar10/"
+    # imgPath = rootPath + "_Images/"
+    # test_fListPath = rootPath + "test.txt"
+
+    # model_name = "WRN_N4_k4"
+    # save_folder = "./weights/allTrain/WRN_N4_k4_drop0.3/bs128_ep200_warm5_lr0.1_gamma0.2_wdecay0.0005_nesterov/"
+    # model_path = os.path.join(save_folder, model_name + "_Best.pth")
+
+    # num_classes = 10
+    # batch_size_test = 100 # data size can be not divisible by batch_size
+    # display_interval = 40 
 
     #--------------------------------------------------------------------------------------------------------#
 
-    ''' Loading and normalizing Custom cifar10 dataset '''
+    ''' Loading and normalizing Custom dataset '''
     transform_test = transforms.Compose(
         [
-         transforms.Resize(size=(60,60)),
-         transforms.CenterCrop(size=(54,54)),
+        #  transforms.Resize(size=(60,60)),
+        #  transforms.CenterCrop(size=(54,54)),
          transforms.ToTensor(),
-        #  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+         transforms.Normalize(args.channel_mean, args.channel_std),
          ])
 
-    test_Dataset = cDataset.ClassifyDataset(test_fListPath, imgPath, transform=transform_test)
-    test_Loader = DataLoader(test_Dataset, batch_size=batch_size_test, shuffle=False, num_workers=2)
+    test_Dataset = cDataset.ClassifyDataset(args.fList_test, args.img_dir, transform=transform_test)
+    test_Loader = DataLoader(test_Dataset, batch_size=args.batch_size_test, shuffle=False, num_workers=2)
     num_data = len(test_Dataset)
-    iterations = np.ceil(num_data/batch_size_test).astype(int)
+    iterations = np.ceil(num_data/args.batch_size_test).astype(int)
 
     ''' Setup model '''
     model_names = sorted( name[6:] for name in models.__dict__
@@ -124,24 +146,40 @@ def main():
     # for m in model_names: 
     #     print(' - ' + m)
         
-    print("Create Model: {}".format(model_name))
+    print("Create Model: {}".format(args.model_name))
 
     net = None
     for model_names_each in model_names:
-        if model_name == model_names_each:
+        if args.model_name == model_names_each:
             net = models.__dict__["Build_" + model_names_each](
-                        num_classes = num_classes,
+                        num_classes = args.num_classes,
                         init_weights = False)
     if net is None:
-        raise ValueError("Not support model -> \"{}\"".format(model_name))
+        raise ValueError("Not support model -> \"{}\"".format(args.model_name))
 
+    if args.model_path == 'None':
+        model_path = os.path.join(args.out_dir, args.model_name + "_Best.pth")
+    else:
+        model_path = args.model_path
+
+    # Load model weight
     state_dict = torch.load(model_path)
-    net.load_state_dict(state_dict)
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        head = k[:7]
+        if head == 'module.':
+            name = k[7:]  # remove `module.`
+        else:
+            name = k
+        new_state_dict[name] = v
+
+    net.load_state_dict(new_state_dict)
     net.eval()
 
     print(' - Finished loading model!')
 
     ''' Setup GPU '''
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
         print(" - GPU is available -> use GPU")
@@ -151,7 +189,7 @@ def main():
 
     net.to(device)
 
-    test(net, test_Loader, device, display_interval, save_folder,num_classes, iterations)
+    test(net, test_Loader, device, args.display_interval, args.out_dir, args.num_classes, iterations)
 
 #--------------------------------------------------------------------------------------------------------#  
     
---
+++ sample/test.bat
@@ -0,0 +1,24 @@
+set currentPath=%CD%
+set rootPath=D:/Dataset/Classification/cifar10
+set model_name=DenseNet100_k12
+
+@REM #-----------------------------------------------------------------------------------#
+
+cd E:/Coding/Pytorch/project/Classification_Pytorch
+
+python Test.py ^
+    -img_dir %rootPath%/_Images/ ^
+    -fList_test %rootPath%/test.txt ^
+    -num_classes 10 ^
+    -out_dir ./weights/%model_name%/test/ ^
+    -model_path None ^
+    -channel_mean 0.4914 0.4822 0.4465 ^
+    -channel_std 0.2023 0.1994 0.2010 ^
+    -gpu_id 0 ^
+    -model_name %model_name% ^
+    -batch_size_test 100 ^
+    -display_interval 100
+    
+@REM #-----------------------------------------------------------------------------------#
+
+cd %currentPath%
